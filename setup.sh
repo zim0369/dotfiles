63ab41ee853b6a228fb5e6cc1efc506646ed8914
zim0369 <zim@onionmail.org>
2021-06-28 19:45:30 +0530

    easy install using a script

--- setup.sh
+++ setup.sh
@@ -13,3 +13,8 @@ cp -rf tmux.conf ~/.tmux.conf
 cp -rf xinitrc ~/.xinitrc 
 
 sudo cp -rf 40-libinput.conf /etc/X11/xorg.conf.d/
+echo ''
+echo "Please install these packages : feh scrot polybar picom i3lock pulseaudioo rofi \
+    fzf mlocate findutils dragon-drag-and-drop cmus ueberzug cmus zsh gvim tmux \
+    tpm(for tmux) vimplug(vim) pulseaudio alsa-utils font-awesome 5 siji-git and other \
+    fonts listed in the polybar config file"
