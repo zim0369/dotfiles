408c9c9e0b2e9c6107e3be3f1e58e7afe9c5ac47
zim0369 <zim@onionmail.org>
2021-09-01 14:55:35 +0530

    updated

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,6 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -9,9 +11,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	            /*     fg         bg       */
-	[SchemeNorm] = { "#00ffff", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#00ffff", "#000000" },
+	[SchemeSel] = { "#00ffff",  "#000000" },
+	[SchemeOut] = { "#00ffff",  "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
