db45893b53b6f14892c2c4f01c5133f79dc7f796
zim0369 <zim@onionmail.org>
2021-10-21 23:33:28 +0530

    updated

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -1,22 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ "$theme" == "dark" ]]
+if rg -q "dark" /tmp/theme
 then
     cp /home/zim/.github/dotfiles/config/urxvt/urxvt.light /home/zim/.Xresources
     xrdb /home/zim/.Xresources
     cp /home/zim/.github/dotfiles/config/alacritty/alacritty.light.yml /home/zim/.config/alacritty/alacritty.yml
-    sed -i --follow-symlinks "s/colorscheme.*/colorscheme $LIGHTO/g" /home/zim/.vimrc
-    sed -i --follow-symlinks "s/set background=dark/set background=light/g" /home/zim/.vimrc
-    sed -i --follow-symlinks "s/colorscheme.*/colorscheme $LIGHTO/g" /home/zim/.config/nvim/init.vim
-    sed -i --follow-symlinks "s/set background=dark/set background=light/g" /home/zim/.config/nvim/init.vim
-    export theme="light"
+    cat /tmp/light > /home/zim/.vim/theme.vim
+    echo "light" > /tmp/theme
 else
     cp /home/zim/.github/dotfiles/config/urxvt/urxvt.dark /home/zim/.Xresources
     xrdb /home/zim/.Xresources
     cp /home/zim/.github/dotfiles/config/alacritty/alacritty.dark.yml /home/zim/.config/alacritty/alacritty.yml
-    sed -i --follow-symlinks "s/colorscheme.*/colorscheme $DARKO/g" /home/zim/.vimrc
-    sed -i --follow-symlinks "s/set background=light/set background=dark/g" /home/zim/.vimrc
-    sed -i --follow-symlinks "s/colorscheme.*/colorscheme $DARKO/g" /home/zim/.config/nvim/init.vim
-    sed -i --follow-symlinks "s/set background=light/set background=dark/g" /home/zim/.config/nvim/init.vim
-    export theme="dark"
+    cat /tmp/dark > /home/zim/.vim/theme.vim
+    echo "dark" > /tmp/theme
 fi
